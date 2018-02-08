5b0525c8d812760d2449c18514f5203835ae5bef
dragosMartac <dom17@ray01.doc.ic.ac.uk>
2018-02-09 00:28:59 +0200

    Random array test added

--- src/testsuite/Tests.java
+++ src/testsuite/Tests.java
@@ -3,18 +3,47 @@ package testsuite;
 import static junit.framework.TestCase.assertTrue;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.Test;
 import sortings.QuickSort;
 
 public class Tests {
 
+  Random random = new Random();
+
+  //Arrays for testing
+  private Integer[] a = {2, 1, 2, 1, 4, 2, 3};
+  private Integer[] b = {1, 1, 2, 2, 2, 3, 4};
+
+  //Arrays generators
+  private Integer[] integerRandomGenerator(int no, int low, int high){
+
+    return random.ints(no, low, high).boxed().toArray(Integer[]::new);
+  }
+
   @Test
-  public void qsortTest() {
-    Integer[] a = {2, 1, 2, 1, 4, 2, 3};
-    Integer[] b = {1, 1, 2, 2, 2, 3, 4};
+  public void qsortTest1() {
+    long time = System.currentTimeMillis();
     QuickSort.qsort(a);
+    System.out.println("qsortTest1 executed in: " + (System.currentTimeMillis() - time));
 
     assertTrue(Arrays.equals(a, b));
   }
 
+  @Test
+  public void qsortTest2() {
+
+    Integer[] a = integerRandomGenerator(1000000, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    Integer[] b = a.clone();
+    Arrays.sort(b);
+
+
+    long time = System.currentTimeMillis();
+    QuickSort.concurentQsort(a);
+    System.out.println("qsortTest2 executed in: " + (System.currentTimeMillis() - time));
+
+    assertTrue(Arrays.equals(a, b));
+  }
 }
