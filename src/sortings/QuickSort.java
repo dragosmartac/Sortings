9795c0a3a0327fa6176dfcec9e1915510fbf5602
dragosMartac <dom17@ray01.doc.ic.ac.uk>
2018-02-09 00:30:18 +0200

    First version of concurent quick sort

--- src/sortings/QuickSort.java
+++ src/sortings/QuickSort.java
@@ -5,6 +5,45 @@ public class QuickSort {
   public static void qsort(Comparable[] v) {
     qsortHelper(v, 0, v.length - 1);
   }
+  public static void concurentQsort(Comparable[] v) {
+    Thread a = new Thread(() -> qsortHelper(v, 0, v.length / 2));
+    a.start();
+
+    Thread b = new Thread(() -> qsortHelper(v, v.length / 2 + 1, v.length - 1));
+    b.start();
+
+    try {
+      a.join();
+      b.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    //Combine the results
+    Comparable[] u = new Comparable[v.length];
+    int i = 0;
+    int j = v.length / 2 + 1;
+    int ui = 0;
+    while(i <= v.length / 2 && j <= v.length - 1) {
+      if(v[i].compareTo(v[j]) < 0) {
+        u[ui++] = v[i++];
+      }else {
+        u[ui++] = v[j++];
+      }
+    }
+
+    while(i <= v.length / 2 ) {
+      u[ui++] = v[i++];
+    }
+
+    while (j <= v.length - 1) {
+      u[ui++] = v[j++];
+    }
+
+    for(i = 0; i < v.length; ++i) {
+      v[i] = u[i];
+    }
+  }
 
   private static void qsortHelper(Comparable[] v, int l, int r) {
     if(l >= r) {
@@ -18,23 +57,22 @@ public class QuickSort {
   }
 
   private static int partition(Comparable[] v, int l, int r) {
-    int i = l;
-    int j = r - 1;
+    int i = l - 1;
+    int j = r;
     Comparable pivot = v[r];
 
     while(i < j) {
 
+
       //Find the left most element greater than my pivot
-      while(v[i].compareTo(pivot) < 0) {
-        i++;
-      }
+      while(v[++i].compareTo(pivot) < 0);
 
       //Find the right most element lower than my pivot
-      while(v[j].compareTo(pivot) >= 0 && j > i) {
-        j--;
-      }
+      while(v[--j].compareTo(pivot) > 0 && j > i);
 
-      swap(v, i, j);
+      if(i < j) {
+        swap(v, i, j);
+      }
     }
 
     swap(v, i, r);
